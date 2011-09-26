b7f0d4b2366c88e69c4f5ecd3e44869b6f96e8e2
Taylor Savage <tjsavage@glados.stanford.edu>
2011-09-26 16:26:22 -0700

    Initial commit

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
